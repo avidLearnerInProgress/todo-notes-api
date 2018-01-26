f9ef38187efd773626e24db722102a22202eee5c
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-26 16:33:35 +0530

    Added tests

--- server/server.js
+++ server/server.js
@@ -1,44 +1,38 @@
-var {mongoose} = require('./db/mongoose');
-
-//mongoose model
-//mongoose validators
-
-
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
 var app = express();
 
-//middleware
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	//console.log(req.body);
-	var todo =new Todo({
-		text: req.body.text
-	});
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-	todo.save().then( (doc) => {
-		res.send(doc);
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+app.get('/todos', (req,res) =>{
+	Todo.find().then((todos) => {
+
+		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
-	});
+	})
 });
 
 app.listen(3000, () => {
-	console.log('Started on port 3000.');
+  console.log('Started on port 3000');
 });
 
-/*
-Create --> Post HTPP Req
-(send json with text body  to server, the complete text setup on server with id is returned to user in json format)
-*/
-
-
-
-
-
-
+module.exports = {app};
