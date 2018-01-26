9a5d021ef85b00ecdeab5f2c62ef7493017a9a06
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-26 16:48:48 +0530

    Added tests

--- server/tests/server-test.js
+++ server/tests/server-test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+const todos = [{
+  text: 'First todo'
+},{
+  text: 'Second todo'
+}];
+
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {Todo.insertMany(todos)}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -41,11 +49,22 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) =>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    }).end(done);
   });
 });
