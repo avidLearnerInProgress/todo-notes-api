30d7ce848ed26011e4c7201d0615f8f025f5bc63
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-26 18:23:03 +0530

    Added changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -29,6 +32,23 @@ app.get('/todos', (req,res) =>{
 	}, (e) => {
 		res.status(400).send(e);
 	})
+});
+
+app.get('/todos/:id', (req,res) =>{
+
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){  
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) =>{
+    if(!todo){
+      return res.status(400).send();
+    }
+    res.send({todo});
+
+}).catch((e)=> res.status(400).send());
+
+
 });
 
 app.listen(3000, () => {
