33956b4f9378093e5c7843ceff136f7f072b440d
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-26 18:40:55 +0530

    Added tests

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
@@ -51,8 +53,8 @@ app.get('/todos/:id', (req,res) =>{
 
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
--- server/tests/server-test.js
+++ server/tests/server-test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First todo'
 },{
+  _id: new ObjectID(),
   text: 'Second todo'
 }];
 
@@ -67,4 +70,37 @@ describe('GET /todos', () => {
       expect(res.body.todos.length).toBe(2);
     }).end(done);
   });
+});
+
+
+describe('GET /todos/id', () => {
+  it('should return todo doc', (done) =>{
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    }).end(done);
+  });
+
+  it('should return 404 if todo not found', (done) =>{
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+    .get(`/todos/${hexId}`)
+    .expect(400)
+    .end(done);
+
+  });
+
+
+    it('should return 404 for non-object ids', (done) =>{
+  
+    request(app)
+    .get(`/todos/123abc`)
+    .expect(404)
+    .end(done);
+    
+  });
+
 });
