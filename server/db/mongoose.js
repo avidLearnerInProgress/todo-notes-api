4a871bc7dca2d93604b0b7ec9495eeb9955af1cc
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-30 00:09:39 +0530

    Added Changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,3 +4,4 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://chirag:chirag@ds117888.mlab.com:17888/node-api-db');
 
 module.exports = {mongoose};
+
--- server/server.js
+++ server/server.js
@@ -45,8 +45,28 @@ app.get('/todos/:id', (req, res) => {
 
     res.send({todo});
   }).catch((e) => {
-    res.status(400).send();
+    res.status(400).send(e);
   });
+});
+
+
+app.delete('/todos/:id', (req,res) =>{
+
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findOneAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
 });
 
 app.listen(port, () => {
