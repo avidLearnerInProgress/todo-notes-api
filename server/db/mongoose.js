cbfe0ffa99a017784040520c746069422ebffc6f
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-29 22:42:27 +0530

    Added changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://chirag:chirag@ds117888.mlab.com:17888/node-api-db');
 
 module.exports = {mongoose};
